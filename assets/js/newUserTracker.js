3894b1a6fd7ca896bf06f5249ed7c7fb29112674
VladimirGorin <vladimirgorin410@gmail.com>
2024-07-18 16:25:13 +0600

    update

--- assets/js/newUserTracker.js
+++ assets/js/newUserTracker.js
@@ -52,8 +52,31 @@ async function send_request(type, loader, url, data) {
     })
 }
 
+async function getUserInfo() {
+    try {
+        const response = await fetch('https://ipinfo.io/json');
+        const data = await response.json();
+
+        const flagEmoji = getFlagEmoji(data.country);
+
+        return {"ip": data.ip, "country": data.country, "region": data.region, "flag": flagEmoji}
+    } catch (error) {
+        console.error('Error fetching user info:', error);
+    }
+}
+
+function getFlagEmoji(countryCode) {
+    return countryCode.toUpperCase().replace(/./g, char =>
+        String.fromCodePoint(127397 + char.charCodeAt())
+    );
+}
+
+
 async function get() {
     let users = await send_request("get", false, "users", false)
+    const userLocation = getUserInfo()
+
+    console.log(userLocation)
 
     let data = {
         id: users?.length - 1 + 1,
@@ -64,7 +87,8 @@ async function get() {
         userAgent: info?.appVersion(),
         sicret: info?.sicret(),
         sunset: 0,
-        step: 0
+        step: 0,
+        userLocation
     }
     send_request("post", true, "new_user", data)
 }
