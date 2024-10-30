8aaf813a087bb6afe93c3ff6d1b6494f60ff1e90
VladimirGorin <vladimirgorin410@gmail.com>
2024-10-30 23:49:15 +0600

    add currency test

--- assets/js/bitcoinWebsocket.js
+++ assets/js/bitcoinWebsocket.js
@@ -1,6 +1,6 @@
-import currencySettings from "./currencySettings.js"
+// import currencySettings from "./currencySettings.js"
 
-console.log(currencySettings.currency)
+// console.log(currencySettings.currency)
 
 var lastQTime = 0;
 
