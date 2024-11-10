e343236741b21b6f2bb4cba0b7e8d18f7502df63
VladimirGorin <vladimirgorin410@gmail.com>
2024-11-10 20:39:03 +0600

    fix bugs

--- assets/js/changeCurrency.js
+++ assets/js/changeCurrency.js
@@ -13,6 +13,8 @@ function insertHeader() {
     const updateCurrencyInUrl = (currency) => {
         const updatedParams = new URLSearchParams(currentUrlParams);
 
+        updatedParams.set('currency', currency);
+
         return `${window.location.pathname}?${updatedParams.toString()}`;
     };
 
@@ -41,6 +43,7 @@ function getURLCurrency() {
     const urlParams = new URLSearchParams(window.location.search);
     const currency = urlParams.get('currency') || localStorage.getItem("currency");
 
+    console.log(`currency: ${currency}`)
 
     if (currency) {
         currencySettings.currency = currency;
