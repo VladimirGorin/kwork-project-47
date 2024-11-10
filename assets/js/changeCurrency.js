afccb7a2b0d0f1428fdf1efc2c9b4e0bcd519cf7
VladimirGorin <vladimirgorin410@gmail.com>
2024-11-10 20:49:14 +0600

    fix bugs

--- assets/js/changeCurrency.js
+++ assets/js/changeCurrency.js
@@ -67,8 +67,10 @@ function start() {
 
 start();
 
-window.getCurrency = async function (){
+async function getCurrency (){
     await start()
 
     return  localStorage.getItem("currency")
 }
+
+window.getCurrency = getCurrency;
